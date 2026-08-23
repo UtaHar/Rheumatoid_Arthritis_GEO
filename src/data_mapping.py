4e920626d80d71b74eadf3f865a34052b800031c
uta-h <uta-hardt@msn.com>
2026-08-23 11:26:11 +0200

    Display study names instead of locations

--- src/data_mapping.py
+++ src/data_mapping.py
@@ -4,14 +4,15 @@ from data_wrangling import get_df
 
 #df = get_df()
 #df.to_csv('../data/intermediate_df.csv',index=False)
-df = (pd.read_csv('../data/intermediate_df.csv').dropna(subset="altitude")
-#with pd.option_context("display.max_rows", 10):
-#    print(df)
-m = folium.Map(location=[df['latitude'].mean(),df['longitude'].mean()], tiles="OpenStreetMap", zoom_start=2)
+df = pd.read_csv('../data/intermediate_df.csv').dropna(subset="altitude")
+with pd.option_context("display.max_columns", None):
+    print(df)
+m = folium.Map(location=[df['latitude'].mean(),df['longitude'].mean()], tiles="OpenStreetMap", zoom_start=3)
 for i in range(0,len(df)):
    folium.Marker(
       location=[df.iloc[i]['latitude'], df.iloc[i]['longitude']],
-      popup=df.iloc[i]['Location'],
+      popup=df.iloc[i]['Citation'],
    ).add_to(m)
-m.save("test.html")
+m.save("mapped_studies.html")
+
 
--- src/data_wrangling.py
+++ src/data_wrangling.py
@@ -5,6 +5,7 @@ from geopy.extra.rate_limiter import RateLimiter
 geopy.geocoders.options.default_timeout = None
 
 def get_df():
+    print("reading data csv")
     meta = pd.read_csv('../data/GBD-DATA-INPUT-SOURCES-1.zip')
     #pd.set_option('display.max_columns',None)
     #print(meta.head(2))
@@ -12,14 +13,22 @@ def get_df():
     #print(meta.info())
     #print(meta.describe().T)
 
+    print("dropping NA")
     meta.dropna(subset=['Location'])
-    df = pd.DataFrame(meta['Location'].unique(),columns=['Location'])
+
+    print("finding unique citation, location pairs")
+    df = meta[['Citation', 'Location']].drop_duplicates().reset_index(drop=True)
+
+    print("finding locations on map")
     geolocator = Nominatim(user_agent='RA_project')
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
     df['name'] = df['Location'].apply(geocode)
-    df['point'] = df['name'].apply(lambda loc: tuple(loc.point) if loc else None)
 
+    print("formatting data")
+    df['point'] = df['name'].apply(lambda loc: tuple(loc.point) if loc else None)
     df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+
+    print("dropping location NAs")
     df.dropna(subset=['altitude'])
 
     #GPS_meta = meta.set_index('Location').join(df.set_index('Location'))
@@ -28,4 +37,5 @@ def get_df():
     #    print(df)
 
     #return GPS_meta
+    print("returning data frame")
     return df
